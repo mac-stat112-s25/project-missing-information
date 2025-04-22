7b02a592d18da8f0c0bd7943c01a6f410273333a
AshERyan21 <aryan2@macalester.edu>
2025-04-22 16:55:48 -0500

    Updates

--- Trees/app.R
+++ Trees/app.R
@@ -12,37 +12,59 @@ library(leaflet)
 library(readr)
 library(here)
 
-trees <- read_csv(here("data", "trees1.csv"))
+trees <- read_csv(here("src/appx", "Apptrees.csv"))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
     titlePanel("Trees of Macalester"),
+    sidebarLayout(
+      sidebarPanel(
+        checkboxGroupInput("selected_Species", "Choose Species to Display:",
+                           choices = unique(trees$TFSpecies),
+                           selected = unique(trees$TFSpecies))
+      ),
+      mainPanel(
     leafletOutput("leafletMap", height = 600)
+      )
+    )
 )
 
-    
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+  treeIcon <- makeIcon(
+    iconUrl = "https://cdn.pixabay.com/photo/2017/01/31/22/47/tree-2027899_1280.png",
+    iconWidth = 17, iconHeight = 17,
+    iconAnchorX = 7.5, iconAnchorY = 7.5
+  )
 
     output$leafletMap <- renderLeaflet({
         leaflet(trees) |>
         addTiles() |>
-        addAwesomeMarkers(
+        flyTo(
+          lng = mean(trees$x),
+          lat = mean(trees$y),
+          zoom = 15
+        )
+    })
+    
+    observe({
+      filtered <- trees[trees$TFSpecies %in% input$selected_Species, ]
+      leafletProxy("leafletMap") |>
+        clearMarkers() |>
+        addMarkers(
+          data = filtered, 
           lng = ~x,
           lat = ~y,
-          icon = awesomeIcons(
-            icon = 'leaf',
-            iconColor = 'white',
-            markerColor = 'green',
-            library = 'fa'
-          ),
+          icon = treeIcon,
           label = ~`Common Species Name`
-        ) 
-    })
+        )
+       })
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
